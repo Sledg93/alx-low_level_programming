43ed6f1262cdb5b8cfff1bdf58fb372087b6deee
Sledg93 <godwin.udie91@gmail.com>
2023-09-17 11:37:32 -0700

    9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
  * times_table - prints the 9 times table, starting with 0
  */
@@ -21,7 +20,7 @@ _putchar(',');
 _putchar(' ');
 _putchar(' ');
 _putchar(k + '0');
-} 
+}
 else if (k >= 10)
 }
 _putchar(',');
